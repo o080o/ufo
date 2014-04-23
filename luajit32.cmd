66491379abfb0cc202fac9f836455d4571e13906
Alex <o0o.moro.o0o@gmail.com>
2014-04-23 18:45:03 -0500

    Fixed .cmd scripts to not alter env variables after exit.

--- luajit32.cmd
+++ luajit32.cmd
@@ -1,3 +1,7 @@
+@SET OLDLUA_PATH=%LUA_PATH%
 @SET LUA_PATH=%~dp0\?.lua;%LUA_PATH%
+@SET OLDPATH=%PATH%
 @SET PATH=%~dp0;%PATH%
 @"%~dp0bin\Windows\x86\luajit.exe" %*
+@SET PATH=%OLDPATH%
+@SET LUA_PATH=%OLDLUA_PATH%
